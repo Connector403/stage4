6310f4d86f2d11e6dcb000bc7b013964c3234138
Connector403 <44946843+Connector403@users.noreply.github.com>
2020-12-26 21:13:02 +0000

    STILL cannot get the paramater error code 14  WORKING

--- libs/js/script.js
+++ libs/js/script.js
@@ -3,13 +3,15 @@ $(document).ready(function() {
     $.ajax(
       {
         type: "POST",
-        url: "libs/php/findnearbyplacename.php",
+        url: "http://localhost/findnearbyplacename/libs/php/findnearbyplacename.php",
         data: {
-          $lat: $('lat').val(),
-          $lng: $('lng').val()
+          lat: $('#lat').val(),
+          lng: $('#lng').val(),
         },
         success: function(result){
-          $('div1').html(result);
+          console.log(result);
+
+          $('#div1').html(result['data'][0]['distance'])
         }
       },)
   });
--- libs/php/findnearbyplacename.php
+++ libs/php/findnearbyplacename.php
@@ -1,30 +1,37 @@
 <?php
 
 $lat = $_REQUEST['lat'];
-  $lng = $_REQUEST['lng'];
+$lng = $_REQUEST['lng'];
 
 
-  $url = "http://api.geonames.org/findNearbyPlaceNameJSON?formatted=true&lat=". $lat."&lng=".$lng."&username=sherazzi403";
+  ini_set('display_errors' , 'On');
+  error_reporting(E_ALL);
+
+
+  $url = "http://api.geonames.org/findNearbyPlaceNameJSON?formatted=true&lat=7.3&lng=9&username=sherazzi403&style=full";
 
   $ch = curl_init();
 
   curl_setopt($ch, CURLOPT_POST, 1  );
-
   curl_setopt($ch, CURLOPT_URL, $url);
-
-     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
   // curl_setopt($ch, CURLOPT_POSTFIELDS)
 
 
   $result = curl_exec($ch);
+  curl_close($ch);
 
 
   $decode = json_decode($result,true);
 
-  curl_close($ch);
 
-  print_r($decode);
+  $output['data'] = $decode['geonames'];
+
+  header('Content-Type: application/json; charset=UTF-8');
+
+
+  print_r($output);
 
 
 
