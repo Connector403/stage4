6399adbef614624e7602d48f99c0abe226d63b4b
Connector403 <44946843+Connector403@users.noreply.github.com>
2020-12-27 13:01:41 +0000

    Everything is now working, however some lat cord and some lng cord is not producing any results

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,18 +1,38 @@
-$(document).ready(function() {
-  $('buttonRun').click(function() {
+
+  $('#btnRun').click(function() {
+
+
     $.ajax(
       {
-        type: "POST",
-        url: "http://localhost/findnearbyplacename/libs/php/findnearbyplacename.php",
+        method: "POST",
+        url: "libs/php/findnearbyplacename.php",
         data: {
           lat: $('#lat').val(),
           lng: $('#lng').val(),
         },
         success: function(result){
-          console.log(result);
+          console.log(result['data']);
+          // for (i = 0; i < result.length; i++) {
+          //   document.write(result['data'][i]);
+          //
+          // }
 
-          $('#div1').html(result['data'][0]['distance'])
-        }
-      },)
+          $('#div1').html(result['data'][0]['distance']);
+          // $('#timezone').html(result['data'][0][1]['timeZoneId']);
+          $('#countryCode').html(result['data'][0]['countryCode']);
+          $('#countryName').html(result['data'][0]['countryName']);
+          $('#fclName').html(result['data'][0]['fclName']);
+          $('#continentCode').html(result['data'][0]['continentCode']);
+          $('#timezone').html(result['data'][0]['timezone']['timeZoneId']);
+
+
+
+        },
+         error: function(jqXHR, textStatus, errorThrown){
+          // error code
+          console.log("we have reached error ");
+
+
+         }
+      });
   });
-});
