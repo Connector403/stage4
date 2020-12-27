1457363e9b59189283507b9b925f9d4f7e0d300a
Connector403 <44946843+Connector403@users.noreply.github.com>
2020-12-27 13:08:16 +0000

    all files are working as they are supposed to

--- libs/js/script.js
+++ libs/js/script.js
@@ -1,38 +1,31 @@
+$('#btnRun').click(function() {
+  $.ajax(
+    {
+      method: "POST",
+      url: "libs/php/findnearbyplacename.php",
+      data: {
+        lat: $('#lat').val(),
+        lng: $('#lng').val(),
+      },
+      success: function(result){
+        console.log(result['data']);
 
-  $('#btnRun').click(function() {
-
-
-    $.ajax(
-      {
-        method: "POST",
-        url: "libs/php/findnearbyplacename.php",
-        data: {
-          lat: $('#lat').val(),
-          lng: $('#lng').val(),
-        },
-        success: function(result){
-          console.log(result['data']);
-          // for (i = 0; i < result.length; i++) {
-          //   document.write(result['data'][i]);
-          //
-          // }
-
-          $('#div1').html(result['data'][0]['distance']);
-          // $('#timezone').html(result['data'][0][1]['timeZoneId']);
-          $('#countryCode').html(result['data'][0]['countryCode']);
-          $('#countryName').html(result['data'][0]['countryName']);
-          $('#fclName').html(result['data'][0]['fclName']);
-          $('#continentCode').html(result['data'][0]['continentCode']);
-          $('#timezone').html(result['data'][0]['timezone']['timeZoneId']);
+        $('#div1').html(result['data'][0]['distance']);
+        // $('#timezone').html(result['data'][0][1]['timeZoneId']);
+        $('#countryCode').html(result['data'][0]['countryCode']);
+        $('#countryName').html(result['data'][0]['countryName']);
+        $('#fclName').html(result['data'][0]['fclName']);
+        $('#continentCode').html(result['data'][0]['continentCode']);
+        $('#timezone').html(result['data'][0]['timezone']['timeZoneId']);
 
 
 
-        },
-         error: function(jqXHR, textStatus, errorThrown){
-          // error code
-          console.log("we have reached error ");
+      },
+       error: function(jqXHR, textStatus, errorThrown){
+        // error code
+        console.log("we have reached error ");
 
 
-         }
-      });
-  });
+       }
+    });
+});
